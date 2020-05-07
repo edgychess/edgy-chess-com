e8b147199a643c4c7ef45525f9c6de08dfdd88e9
Edgy Chess <64961815+edgychess@users.noreply.github.com>
2020-05-07 13:49:43 +0200

    Add files via upload

---
+++ dist/browser/jocly-allgames.js
@@ -0,0 +1,506 @@
+"use strict";
+
+exports.games = {
+	"classic-chess": {
+		"title": "Chess",
+		"summary": "Regular Orthodox Classic Western Chess",
+		"thumbnail": "knight-thumbnail.png",
+		"module": "chessbase"
+	},
+	"edgy-chess":{
+		"title":"Edgy Chess",
+        "summary": "Chess along the edges by Edge and Marc Bernard (2017)",
+        "thumbnail": "edgy-chess-thumb.png",
+		"module":"chessbase"
+	},
+	"losing-chess": {
+		"title": "Losing Chess",
+		"summary": "Also known as Antichess, Suicide Chess, Giveaway Chess, ...",
+		"thumbnail": "knight-inv-thumbnail.png",
+		"module": "chessbase"
+	},
+	"xiangqi": {
+		"title": "Xiangqi",
+		"summary": "Chinese Chess",
+		"thumbnail": "xiangqi-thumb.png",
+		"module": "chessbase"
+	},
+	"gardner-chess": {
+		"title": "Gardner MiniChess",
+		"summary": "Gardner 5x5 minichess (1969)",
+		"thumbnail": "gardner-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"mini4x4-chess": {
+		"title": "Mini Chess 4x4",
+		"summary": "4x4 mini chess variant",
+		"thumbnail": "mini4x4-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"mini4x5-chess": {
+		"title": "Mini Chess 4x5",
+		"summary": "4x5 mini chess variant",
+		"thumbnail": "mini4x5-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"micro4x5-chess": {
+		"title": "Micro Chess",
+		"summary": "4x5 chess variant by Glimne (1997)",
+		"thumbnail": "micro4x5-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"baby-chess": {
+		"title": "Baby Chess",
+		"summary": "5x5 Baby chess",
+		"thumbnail": "baby-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"malett-chess": {
+		"title": "Malett Chess",
+		"summary": "5x5 chess variant by Jeff Malett",
+		"thumbnail": "malett-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"los-alamos-chess": {
+		"title": "Los Alamos Chess",
+		"summary": "6x6 chess variant",
+		"thumbnail": "los-alamos-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"attack-chess": {
+		"title": "Chess Attack",
+		"summary": "5x6 chess variant",
+		"thumbnail": "attack-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"courier-chess": {
+		"title": "Courier Chess",
+		"summary": "12x8 chess (12th century)",
+		"thumbnail": "courier-thumb.png",
+		"module": "chessbase"
+	},
+	"makruk": {
+		"title": "Makruk",
+		"summary": "Thai Chess",
+		"thumbnail": "mk-thumb.png",
+		"module": "chessbase"
+	},
+	"shako-chess": {
+		"title": "Shako",
+		"summary": "10x10 Chess",
+		"thumbnail": "shako-thumb.png",
+		"module": "chessbase"
+	},
+	"shatranj-chess": {
+		"title": "Shatranj",
+		"summary": "Ancient Chess",
+		"thumbnail": "shatranj-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"raumschach": {
+		"title": "Raumschach",
+		"summary": "5x5x5 Chess",
+		"thumbnail": "raumschach-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"glinski-chess": {
+		"title": "Glinski Chess",
+		"summary": "Hexagonal Chess",
+		"thumbnail": "glinski-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"brusky-chess": {
+		"title": "Brusky Chess",
+		"summary": "Hexagonal Chess",
+		"thumbnail": "brusky-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"devasa-chess": {
+		"title": "De Vasa Chess",
+		"summary": "Hexagonal Chess",
+		"thumbnail": "devasa-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"mccooey-chess": {
+		"title": "McCooey Chess",
+		"summary": "Hexagonal Chess",
+		"thumbnail": "mccooey-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"shafran-chess": {
+		"title": "Shafran Chess",
+		"summary": "Hexagonal Chess",
+		"thumbnail": "shafran-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"circular-chess": {
+		"title": "Modern Circular Chess",
+		"summary": "Chess on a ring",
+		"thumbnail": "circular-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"byzantine-chess": {
+		"title": "Byzantine Chess",
+		"summary": "10th century circular Chess",
+		"thumbnail": "byzantine-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"3dchess": {
+		"title": "3D Chess",
+		"summary": "6x8x3 Chess",
+		"thumbnail": "3dchess-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"cylinder-chess": {
+		"title": "Cylinder Chess",
+		"summary": "Cylinder Chess",
+		"thumbnail": "cylinder-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"rollerball-chess": {
+		"title": "Rollerball Chess",
+		"summary": "Chess variant on an unusual board",
+		"thumbnail": "rollerball-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"chess960": {
+		"title": "Chess 960",
+		"summary": "Chess from randomized positions",
+		"thumbnail": "chess960-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"metamachy-chess": {
+		"title": "Metamachy",
+		"summary": "Chess on 12x12 with fairy pieces",
+		"thumbnail": "metamachy-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"capablanca-chess": {
+		"title": "Capablanca Chess",
+		"summary": "Chess on 10x8 (1920)",
+		"thumbnail": "capablanca-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"carrera-chess": {
+		"title": "Carerra Chess",
+		"summary": "Chess on 10x8 (1617)",
+		"thumbnail": "carrera-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"gothic-chess": {
+		"title": "Gothic Chess",
+		"summary": "Chess on 10x8 (2000)",
+		"thumbnail": "gothic-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"janus-chess": {
+		"title": "Janus Chess",
+		"summary": "Chess on 10x8 (1978)",
+		"thumbnail": "janus-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"grand-chess": {
+		"title": "Grand Chess",
+		"summary": "Chess on 10x10 (1984)",
+		"thumbnail": "grand-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"modern-chess": {
+		"title": "Modern Chess",
+		"summary": "Chess on 9x9 (1968)",
+		"thumbnail": "modern-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"chancellor-chess": {
+		"title": "Chancellor Chess",
+		"summary": "Chess on 9x9 (1887)",
+		"thumbnail": "chancellor-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"wildebeest-chess": {
+		"title": "Wildebeest Chess",
+		"summary": "Chess on 11x10 (1987)",
+		"thumbnail": "wildebeest-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"smess": {
+		"title": "Smess",
+		"summary": "The Ninny's Chess (1970)",
+		"thumbnail": "smess-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"demi-chess": {
+		"title": "Demi-Chess",
+		"summary": "4x8 chess variant by Peter Krystufek (1986)",
+		"thumbnail": "demi-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"romanchenko-chess": {
+		"title": "Romanchenko's Chess",
+		"summary": "Shifted 8x8 chess variant by V. Romanchenko",
+		"thumbnail": "romanchenko-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"amazon-chess": {
+		"title": "Amazon Chess",
+		"summary": "18th century, Russia",
+		"thumbnail": "amazon-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"dukerutland-chess": {
+		"title": "Duke of Rutland Chess",
+		"summary": "Chess on 14x10 (1747)",
+		"thumbnail": "dukerutland-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"gustav3-chess": {
+		"title": "Gustav III Chess",
+		"summary": "Gustav Johan Billberg, 1839",
+		"thumbnail": "gustav3-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"hyderabad-chess": {
+		"title": "Hyderabad Decimal Chess",
+		"summary": "Shir Muhammad Khan Iman, 1797-1798",
+		"thumbnail": "hyderabad-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"kaisergame-chess": {
+		"title": "Kaiserspiel",
+		"summary": "Tressau, 1840",
+		"thumbnail": "kaisergame-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"sultangame-chess": {
+		"title": "Sultanspiel",
+		"summary": "Tressau, 1840",
+		"thumbnail": "sultangame-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"reformed-courier-chess": {
+		"title": "Reformed Courierspiel",
+		"summary": "Clément Bégnis, 2011",
+		"thumbnail": "reformed-courier-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"tutti-frutti-chess": {
+		"title": "Tutti-Frutti Chess",
+		"summary": "Ralph Betza et Philip Cohen, 1978-79",
+		"thumbnail": "tutti-frutti-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"sweet16-chess": {
+		"title": "Sweet 16 Chess",
+		"summary": "A huge 16x16 Chess Variant",
+		"thumbnail": "sweet16-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"tera-chess": {
+		"title": "Terachess",
+		"summary": "Chess on 16x16 with fairy pieces",
+		"thumbnail": "terachess-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"giga-chess": {
+		"title": "Gigachess",
+		"summary": "Chess on 14x14 with fairy pieces",
+		"thumbnail": "gigachess-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"leychessalpha-chess": {
+		"title": "LeyChessAlpha",
+		"summary": "Chess on 12x12 with fairy pieces",
+		"thumbnail": "metamachy-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"wild-tamerlane-chess": {
+		"title": "Wild Tamerlane",
+		"summary": "Chess on 11x11 with fairy pieces",
+		"thumbnail": "wild-tamerlane-thumb.png",
+		"module": "chessbase",
+		"obsolete": false
+	},
+	"alquerque-roman": {
+		"title": "Roman Alquerque",
+		"summary": "Alquerque as played in France and England.",
+		"thumbnail": "alquerque-roman-thumb3d.png",
+		"module": "checkers"
+	},
+	"alquerque-arabic": {
+		"title": "Arabic Alquerque",
+		"summary": "Alquerque as played in Middle-East and Spain.",
+		"thumbnail": "alquerque-arabic-thumb.png",
+		"module": "checkers"
+	},
+	"draughts": {
+		"title": "International Draughts",
+		"summary": "Rules for draughts as played in worldwide competitions.",
+		"thumbnail": "draughts-thumb3d.png",
+		"module": "checkers"
+	},
+	"english-draughts": {
+		"title": "English Draughts",
+		"summary": "A popular version of checkers on a 8x8 board.",
+		"thumbnail": "draughts8-thumb3d.png",
+		"module": "checkers"
+	},
+	"suicide-checkers": {
+		"title": "Suicide checkers",
+		"summary": "A.k.a Giveaway checkers or Anti-checkers",
+		"thumbnail": "draughts8-suicide-thumb3d.png",
+		"module": "checkers"
+	},
+	"brazilian-draughts": {
+		"title": "Brazilian Draughts",
+		"summary": "Same as international checkers on a 8x8 board.",
+		"thumbnail": "draughts8-thumb3d.png",
+		"module": "checkers"
+	},
+	"spanish-draughts": {
+		"title": "Spanish Draughts",
+		"summary": "Same as international checkers on a 8x8 board, no backward capture.",
+		"thumbnail": "draughts8-thumb3d.png",
+		"module": "checkers"
+	},
+	"german-draughts": {
+		"title": "German Draughts",
+		"summary": "Checkers according to German Draughts rules.",
+		"thumbnail": "draughts8-thumb3d.png",
+		"module": "checkers"
+	},
+	"thai-draughts": {
+		"title": "Thai Draughts",
+		"summary": "Checkers according to Thai rules.",
+		"thumbnail": "draughts8-thumb3d.png",
+		"module": "checkers"
+	},
+	"kids-draughts": {
+		"title": "Kids Draughts",
+		"summary": "A version for kids of checkers on a 6x6 board.",
+		"thumbnail": "kidsdraughts-thumb3d.png",
+		"module": "checkers"
+	},
+	"turkish-draughts": {
+		"title": "Turkish Draughts",
+		"summary": "A 8x8 checkers on straight lines.",
+		"thumbnail": "turkish-thumb3d.png",
+		"module": "checkers"
+	},
+	"fourinarow": {
+		"title": "Four In A Row",
+		"summary": "Four In A Row game",
+		"thumbnail": "fiar-thumb.png",
+		"module": "fourinarow"
+	},
+	"fourinarow96": {
+		"title": "Four In A Row 9x6",
+		"summary": "Four In A Row on a 9x6 board",
+		"thumbnail": "fiar-thumb.png",
+		"module": "fourinarow"
+	},
+	"fiarpopout": {
+		"title": "Popout",
+		"summary": "Four In A Row with removals",
+		"thumbnail": "fiar-thumb.png",
+		"module": "fourinarow"
+	},
+	"fiveinarow": {
+		"title": "Five In A Row",
+		"summary": "Variant of Four In A Row",
+		"thumbnail": "fiar-thumb.png",
+		"module": "fourinarow"
+	},
+	"torus4": {
+		"title": "Torus 4",
+		"summary": "Four In A Row on a cylinder",
+		"thumbnail": "fiar-torus-thumb.png",
+		"module": "fourinarow"
+	},
+	"reversi": {
+		"title": "Annexation",
+		"summary": "Also called Reversi or Othello",
+		"thumbnail": "thumb-reversi.png",
+		"module": "reversi"
+	},
+	"reversi6": {
+		"title": "Annexation 6",
+		"summary": "Reversi/Othello rules on a 6x6 board",
+		"thumbnail": "thumb-reversi6.png",
+		"module": "reversi"
+	},
+	"reversi4": {
+		"title": "Annexation 4",
+		"summary": "Reversi/Othello rules on a 4x4 board",
+		"thumbnail": "thumb-reversi4.png",
+		"module": "reversi"
+	},
+	"reversi10": {
+		"title": "Annexation 10",
+		"summary": "Reversi/Othello rules on a 10x10 board",
+		"thumbnail": "thumb-reversi10.png",
+		"module": "reversi"
+	},
+	"reversicross": {
+		"title": "Annexation Cross",
+		"summary": "Reversi/Othello rules on a cross-shaped board",
+		"thumbnail": "thumb-cross.png",
+		"module": "reversi"
+	},
+	"reversicross10": {
+		"title": "Annexation Cross 10",
+		"summary": "Reversi/Othello rules on a 10x10 cross-shaped board",
+		"thumbnail": "thumb-cross10.png",
+		"module": "reversi"
+	},
+	"reversiturnover": {
+		"title": "Annexation Turn-Over",
+		"summary": "Reversi/Othello rules on a 10x10 octogonal-shaped board",
+		"thumbnail": "thumb-turnover.png",
+		"module": "reversi"
+	}
+};
